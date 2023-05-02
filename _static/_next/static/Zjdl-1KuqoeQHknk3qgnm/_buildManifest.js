a8d97d7f6f7a58b568086c71fdcc4001fd33c280
Artedys <artedys@gmail.com>
2023-05-02 16:32:30 +0200

    adding export command

---
+++ _static/_next/static/Zjdl-1KuqoeQHknk3qgnm/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-2281f6b5c8ef6c03.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
